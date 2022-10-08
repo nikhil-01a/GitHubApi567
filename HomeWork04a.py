f83bd0bc579e7c348f71f273b186ebf2207c8e87
nikhilparmar <nkumarg@stevens.edu>
2022-10-08 02:39:53 -0400

    modified code

--- HomeWork04a.py
+++ HomeWork04a.py
@@ -1,20 +1,29 @@
+"""
+@author: Nikhil Kumar G
+Program: This program is used to take a 'github user id' as an input from the user and give the name of the repository and the number of commits in those repositories as output.
+
+"""
+#importing modules
 import json
 import requests
 
+#seperate function to retrieve and return the number of commits
+def countcommits(username,repo):
+    urlc = "https://api.github.com/repos/%s/%s/commits"% (username,repo)
+    commit_count = requests.get(urlc).json()
+    count = len(commit_count)
+    return count
+
+#Main function to retrieve and return the name of the repositories
 def Githubid():
     username = input('Enter your github user id: ')
     url = "https://api.github.com/users/%s/repos"%username
-    
-
-    #print(url)
     repo_name = requests.get(url).json()
-
-    for project in repo_name:
-        repo = f"{project['name']}"
-        urlc = "https://api.github.com/repos/%s/%s/commits"% (username,repo)
-        print(f"Repo: {project['name']} Number of Commits: {len(urlc)}")
-    #print(user_data)
-    #print('Repo: %s Number of commits: %s')
     
+    for project in repo_name:    
+        repo = f"{project['name']}"
+        print(f"Repo: {repo}, Number of Commits: {countcommits(username,repo)}")
+
+#Calling main function  
 Githubid()
 
